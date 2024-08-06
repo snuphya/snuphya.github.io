1e48cd0908392fb2c729b368a2f5f207ad6453d6
snuphya <physics2020@snu.ac.kr>
2024-08-06 21:40:09 +0900

    .

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -194,7 +194,7 @@
   /**
    * Intro type effect
    */
-  <script src="C:/Users/c5517/OneDrive/Desktop/github/snuphya.github.io/docs/assets/vendor/typed.js/typed.umd.js"></script>
+  <script type="module" src="C:/Users/c5517/OneDrive/Desktop/github/snuphya.github.io/docs/assets/vendor/typed.js/typed.umd.js"></script>
   const typed = select('.typed')
   if (typed) {
     let typed_strings = typed.getAttribute('data-typed-items')
