c66a9d421bfbb3637eb2e95980781634d40b0439
snuphya <physics2020@snu.ac.kr>
2024-08-06 21:17:08 +0900

    .

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -5,10 +5,10 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
-import Typed from 'C:/Users/c5517/OneDrive/Desktop/github/snuphya.github.io/docs/assets/vendor/typed.js/typed.umd.js';
+// import Typed from 'C:/Users/c5517/OneDrive/Desktop/github/snuphya.github.io/docs/assets/vendor/typed.js/typed.umd.js';
 (function() {
   "use strict";
-
+  
   /**
    * Easy selector helper function
    */
@@ -195,19 +195,19 @@ import Typed from 'C:/Users/c5517/OneDrive/Desktop/github/snuphya.github.io/docs
    * Intro type effect
    */
 
-  const typed = select('.typed')
-  if (typed) {
-    let typed_strings = typed.getAttribute('data-typed-items')
-    typed_strings = typed_strings.split(',')
+  // const typed = select('.typed')
+  // if (typed) {
+  //   let typed_strings = typed.getAttribute('data-typed-items')
+  //   typed_strings = typed_strings.split(',')
     
-    new Typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
-    });
-  }
+  //   new Typed('.typed', {
+  //     strings: typed_strings,
+  //     loop: true,
+  //     typeSpeed: 100,
+  //     backSpeed: 50,
+  //     backDelay: 2000
+  //   });
+  // }
 
   /**
    * Initiate portfolio lightbox 
