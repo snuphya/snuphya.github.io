317ac85370dfc61cd3d492d482354979ec4653a4
snuphya <physics2020@snu.ac.kr>
2024-08-13 23:16:50 +0900

    expertise and skills initial

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -253,6 +253,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }, true)
 
+
   /**
    * Scroll with ofset on page load with hash links in the url
    */
@@ -279,31 +280,9 @@ document.addEventListener('DOMContentLoaded', function() {
       backDelay: 2000
     });
   }
+//
 
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
+//
 
   /**
    * Portfolio details slider
